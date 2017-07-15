3285dce4a1c97bdb7bcb909bf347aa6e9b676011
Administrator <tianzhiyuanemail@163.com>
2017-07-15 13:36:42 +0800

    Initial commit

---
+++ b2c-member-service-web/src/main/java/com/b2c/B2cMemberServiceWebApplication.java
@@ -0,0 +1,12 @@
+package com.b2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2cMemberServiceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2cMemberServiceWebApplication.class, args);
+	}
+}
